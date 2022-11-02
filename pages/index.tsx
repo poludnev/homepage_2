9007398f0d16f1a94b5166deb4bd53e798bbe3d1
poludnev <poludnev.roman@gmail.com>
2022-11-02 19:28:30 +0300

    Added pre-commit lint and test

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Roman's Homepage</title>
+        <title>Roman&apos;s Homepage</title>
         <meta name="description" content="Roman's Homepage" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
