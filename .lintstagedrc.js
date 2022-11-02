6752fc606e32d7384c1dc1cd37d2fcabbc9b1de2
poludnev <poludnev.roman@gmail.com>
2022-11-02 21:46:22 +0300

    updated lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
+const buildEslintCommand = (filenames) => [
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+    .join(' --file ')} `,
+  `prettier --write "pages/**/${filenames}"`,
+  `prettier --write "components/**/${filenames}"`,
+  `prettier --write "*.{json,md}"`,
+];
 
 module.exports = {
   '*.{js,jsx,ts,tsx,md,scss}': [buildEslintCommand],
