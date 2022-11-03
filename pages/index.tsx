81d7dcfb5cda31da6500a02d2a5b2e0c4401b6c0
poludnev <poludnev.roman@gmail.com>
2022-11-04 01:54:24 +0300

    Added random background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import Head from 'next/head';
+import { useEffect, useRef } from 'react';
+
+import cn from 'classnames';
 
 import { useTranslation } from 'react-i18next';
+
+import { getRandomOfSix } from '../utils';
 import styles from './index.module.scss';
+import classNames from 'classnames';
 
 export default function Home() {
   const { t } = useTranslation();
+
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(img/bg-pic-${getRandomOfSix()}.jpeg)`,
+      }}
+    >
       <Head>
         <title>Roman&apos;s Homepage</title>
         <meta name="description" content="Roman's Homepage" />
